15a22cc434894158c388cdfde6939499d3591a88
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-05-13 03:32:29 -0300

    Switch between windows and initial CRUD

--- server/main.py
+++ server/main.py
@@ -1,13 +1,16 @@
 import Pyro4
+import argparse
 import server
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ns_address", help="name service address")
+    args = parser.parse_args()
+
     Pyro4.Daemon.serveSimple(
-    {
-        server.CountingSticks(): "countingsticks"
-    },
-    ns=True, verbose=True, host="192.168.1.111")
+        {server.CountingSticks(): "countingsticks"},
+        ns=True, verbose=True, host=args.ns_address)
 
 if __name__ == '__main__':
     main()
--- server/server.py
+++ server/server.py
@@ -1,18 +1,57 @@
+# -*- coding: utf-8 -*-
+import pymongo
+import bson
 
 
 class CountingSticks(object):
 
     def __init__(self):
-        self.users = {}
+        pass
 
     def register(self, username, email, password):
-        print(username, email, password)
-        self.users[username] = {'username': username, 'email': email, 'password': password }
-        print("Registered")
-
-        print('Current users: ', self.users)
+        mongo_client = pymongo.MongoClient()
+        db = mongo_client.countingsticks
+        new_user = {'username': username, 'email': email, 'password': password}
+        db.user.insert_one(new_user)
+        mongo_client.close()
 
     def login(self, username, password):
-        if username in self.users:
-            found_user = self.users[username]
-            return found_user['password'] == password
+        mongo_client = pymongo.MongoClient()
+        db = mongo_client.countingsticks
+        found_user = db.user.find_one({'username': username, 'password': password})
+        mongo_client.close()
+
+        return found_user is not None
+
+    def create_room(self, name, min_players, max_players):
+        mongo_client = pymongo.MongoClient()
+        db = mongo_client.countingsticks
+        new_room = {'name': name, 'min_players': min_players, 'max_players': max_players,
+                    'current_players': [], 'playing': False}
+        result = db.room.insert_one(new_room)
+        mongo_client.close()
+
+        return result.inserted_id
+
+    def close_room(self, room_id, username):
+        # check if the user who is closing is who created
+        # check if a game isn' running
+        pass
+
+    def list_rooms_ids(self):
+        mongo_client = pymongo.MongoClient()
+        db = mongo_client.countingsticks
+
+        rooms = []  # change to get only id
+        for room in db.room.find({}):
+            rooms.append(str(room['_id']))
+
+        mongo_client.close()
+        return rooms
+
+    def room_state(self, room_id):
+        mongo_client = pymongo.MongoClient()
+        db = mongo_client.countingsticks
+        room = db.room.find_one({'_id': bson.objectid.ObjectId(room_id.strip())})
+        mongo_client.close()
+        return room
