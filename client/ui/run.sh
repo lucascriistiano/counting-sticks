e9cf49b22a87d5f31de70ecc5053c0807767fbd8
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-05-13 21:44:45 -0300

    Widgets module move

--- client/ui/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pyuic5 -o out/login.py login.ui
-pyuic5 -o out/register.py register.ui
-pyuic5 -o out/new_room.py new_room.ui
-pyuic5 -o out/room_list.py room_list.ui
-# pyuic5 -o out/game.py game.ui
