c3080d6026bf64241de922f7cca5c79c937af039
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-05-14 23:51:01 -0300

    Partial send message and join room

--- server/server.py
+++ server/server.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
 import pymongo
 import bson
+from datetime import datetime
+import threading
 
 
 class CountingSticks(object):
 
     def __init__(self):
-        pass
+        self.lock = threading.Lock()
 
     def register(self, username, email, password):
         mongo_client = pymongo.MongoClient()
-        db = mongo_client.countingsticks
+        user_collection = mongo_client.countingsticks.user
         new_user = {'username': username, 'email': email, 'password': password}
-        db.user.insert_one(new_user)
+        user_collection.insert_one(new_user)
         mongo_client.close()
 
     def login(self, username, password):
         mongo_client = pymongo.MongoClient()
-        db = mongo_client.countingsticks
-        found_user = db.user.find_one({'username': username, 'password': password})
+        user_collection = mongo_client.countingsticks.user
+        found_user = user_collection.find_one({'username': username, 'password': password})
         mongo_client.close()
 
         return found_user is not None
 
     def list_rooms_ids(self):
         mongo_client = pymongo.MongoClient()
-        db = mongo_client.countingsticks
+        room_collection = mongo_client.countingsticks.room
 
         rooms = []  # change to get only id
-        for room in db.room.find({}):
+        for room in room_collection.find({}):
             rooms.append(str(room['_id']))
 
         mongo_client.close()
@@ -36,17 +38,17 @@ class CountingSticks(object):
 
     def room_state(self, room_id):
         mongo_client = pymongo.MongoClient()
-        db = mongo_client.countingsticks
-        room = db.room.find_one({'_id': bson.objectid.ObjectId(room_id.strip())})
+        room_collection = mongo_client.countingsticks.room
+        room = room_collection.find_one({'_id': bson.objectid.ObjectId(room_id.strip())})
         mongo_client.close()
         return room
 
     def create_room(self, name, min_players, max_players):
         mongo_client = pymongo.MongoClient()
-        db = mongo_client.countingsticks
+        room_collection = mongo_client.countingsticks.room
         new_room = {'name': name, 'min_players': min_players, 'max_players': max_players,
-                    'current_players': [], 'playing': False}
-        result = db.room.insert_one(new_room)
+                    'current_players': [], 'playing': False, 'messages': []}
+        result = room_collection.insert_one(new_room)
         mongo_client.close()
 
         return result.inserted_id
@@ -56,13 +58,36 @@ class CountingSticks(object):
         # check if a game isn't running
         pass
 
-    def send_message(self, room_id, message):
-        # print message and add in message list
-        print(message)
+    def send_message(self, room_id, username, message):
+        mongo_client = pymongo.MongoClient()
+        room_collection = mongo_client.countingsticks.room
+        room = room_collection.find_one({'_id': bson.objectid.ObjectId(room_id.strip())})
 
-    def join_room(self):
-        # join room
-        pass
+        message_dict = {'datetime': datetime.now(), 'username': username, 'message': message}
+        room['messages'].append(message_dict)
+        room_collection.save(room)
+
+        mongo_client.close()
+
+    def join_room(self, room_id, username):
+        self.lock.acquire()
+        try:
+            mongo_client = pymongo.MongoClient()
+            room_collection = mongo_client.countingsticks.room
+            room = room_collection.find_one({'_id': bson.objectid.ObjectId(room_id.strip())})
+
+            current_players = room['current_players']
+            if len(current_players) < room['max_players']:
+                if username not in current_players:
+                    room['current_players'].append(username)
+                    room_collection.save(room)
+                    mongo_client.close()
+                return True
+            else:
+                return False
+        finally:
+            self.lock.release()
+            print('Released')
 
     def start_game(self, room_id):
         # start a new game in the room
